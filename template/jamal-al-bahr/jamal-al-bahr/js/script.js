363a5083757067a2e605f30922d39c488bd85150
donboscoprompttech <donbosco@prompttech.ae>
2021-07-24 21:44:22 +0400

    changes added

--- template/jamal-al-bahr/jamal-al-bahr/js/script.js
+++ template/jamal-al-bahr/jamal-al-bahr/js/script.js
@@ -10,4 +10,31 @@ jQuery(document).ready(function(){
 	    }
 	});
 
+
+	//CATEGORY CAROUSEL
+	$('#categoryCarousel').owlCarousel({
+		loop:true,
+		margin: 14,
+		nav:true,
+		dots: true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		responsive:{
+			0:{
+				items:2,
+			},
+			768:{
+				items:2,
+				margin: 30,
+			},
+			992:{
+				items:3,
+				margin: 30,
+			},
+			1200:{
+				items:5,
+				margin: 30,
+			}
+		}
+	});
+
 }); //Script End
